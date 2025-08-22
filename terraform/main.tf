e19ef50a684deda8eb7031b9a3b202ae2455e18f
Larry B <larrykbanks21@gmail.com>
2025-08-22 14:37:37 -0400

    infra: associating public key and security group with ec2 instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,18 +6,32 @@ data "aws_ami" "fedora" {
   most_recent = true
 
   filter {
-    name = "name"
+    name   = "name"
     values = [" (SupportedImages) - Fedora-Cloud-Base-AmazonEC2.x86_64-42-1.1 - *"]
   }
 
   owners = ["679593333241"]
 }
 
+data "aws_key_pair" "app_public_key" {
+  key_name = "app_key"
+}
+
+data "aws_security_group" "apps" {
+  name = "Fedora 42 (Fedora Cloud 42) - Support by SupportedImages-20250804-AutogenByAWSMP--1"
+}
+
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.fedora.id
   instance_type = "t2.micro"
+  key_name      = data.aws_key_pair.app_public_key.key_name
+  vpc_security_group_ids = [ data.aws_security_group.apps.id ]
 
   tags = {
     Name = "devbs"
   }
+}
+
+output "vm_public_ip" {
+  value = aws_instance.app_server.public_ip
 }
